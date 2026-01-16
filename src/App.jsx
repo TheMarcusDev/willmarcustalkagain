0312f289710876ce2d4794f5eab689814e97f07a
Marcus Henrique <marcushenrique@MacBook-Pro-de-Marcus.local>
2026-01-16 18:24:06 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./App.css";
+import morse from "./assets/audio/stage1.wav";
+
+import Countdown from "react-countdown";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [stage, setStage] = useState(0);
+
+  const handleSubmit = () => {
+    if (
+      inputValue.toLowerCase().replaceAll(" ", "") ===
+      Secrets[stage].toLowerCase().replaceAll(" ", "")
+    ) {
+      setStage(stage + 1);
+    } else {
+      alert(WrongMessage[stage]);
+    }
+    setInputValue("");
+    document.querySelector("input").value = "";
+  };
+
+  const Prompts = [
+    "Can you see it?",
+    "Can you hear it?",
+    "This is the answer.",
+    "Continues in:",
+  ];
+
+  const Secrets = [
+    "The end of HTG",
+    "Maybe Marcus is hexed",
+    "54 68 69 73 20 69 73 20 74 68 65 20 61 6e 73 77 65 72 2e",
+  ];
+
+  const WrongMessage = [
+    "You're not looking hard enough.",
+    "Most orders right served empty.",
+    "The answer is right in front of you. Maybe you should be hexed too.",
+  ];
+
+  return (
+    <div className="App">
+      <div className="centered-container">
+        <h2>{Prompts[stage]}</h2>
+        {stage == 0 && <secret>{Secrets[stage]}</secret>}
+        {stage == 1 && <audio src={morse} autoPlay />}
+        {stage == 3 && <Countdown date={"2026-01-17T20:00:00Z"} />}
+        {stage < 3 && (
+          <>
+            <input
+              type="text"
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
